7bddf0d4954a515c5dacfafeb16c8dde313fe0af
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-06-13 14:06:58 +0200

    Add class 'active' to the clicked link js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
+    const clickedElement = this;
+    console.log('clickedElement (with plus): ' + clickedElement);
+    console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -19,10 +20,12 @@ const titleClickHandler = function(event){
             activeLink.classList.remove('active');
         }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
 
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .post.active');
 
